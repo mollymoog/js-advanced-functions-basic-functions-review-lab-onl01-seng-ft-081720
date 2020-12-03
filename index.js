1448569ad096b2d4b7ab045cc45554598fcef3eb
mollymoog <mmkover@gmail.com>
2020-12-03 16:18:43 -0500

    commit tree d43d9db2c11f5a9a6c9319445e02ff91c717190b

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Your code here
+
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity="go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(excl="*") {
+    return function(param="special") {
+        return `You are ${excl}${param}${excl}!`;
+    }
+}
+
+let Calculator = {
+    add: function(a, b) {
+        return a + b;
+    },
+
+    subtract: function(a, b) {
+        return a - b;
+    },
+
+    multiply: function(a, b) {
+        return a * b;
+    },
+
+    divide: function(a, b) {
+        return a / b;
+    }
+}
+
+function actionApplyer(int, arrFunc) {
+    let r = int
+    
+    for (let i = 0; i < arrFunc.length; i++) {
+       r = arrFunc[i](r);
+    }
+    return r;
+}
